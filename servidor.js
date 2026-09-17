47b3f0f02427a0a0457aaa222297872202074006
MELISSA BARBOSA LINS <26171667@edu119.local>
2026-09-17 11:56:07 -0300

    Salvando Alterações

--- servidor.js
+++ servidor.js
@@ -28,6 +28,10 @@ app.post("/alunos/cadastrar", (req, res)=>{
     // console.log("Nome:" + nome);
     // console.log(`Curso: ${curso}`);
 
+    if(!nome || !curso){
+        return res.status(400).json({msg: "Nome e curso são obrigatórios"});
+    }
+
     const id = ALUNOS.length > 0 ? ALUNOS[ALUNOS.length - 1].id + 1 : 1;
     // const id = ALUNOS.length > 0 ? Math.max(...ALUNOS.map(aluno => aluno.id)) + 1 : 1;
     const novoAluno = {
@@ -39,9 +43,24 @@ app.post("/alunos/cadastrar", (req, res)=>{
     res.status(201).json({msg: "Aluno cadastrado com sucesso"});
 });
 
+app.get("/alunos/:valor", (req,res)=>{
+    const valor = Number(req.params.valor);
+
+    const aluno = ALUNOS.find(aluno => aluno.id === valor);
+
+    if(!aluno){
+        return res.status(404).json({msg: "Aluno não encontrado"});
+    }
+
+    res.status(200).json(aluno);
+
+});
 
 const PORTA = 3000;
 app.listen(PORTA,()=>{
     console.log("Servidor iniciado com sucesso");
     console.log(`http://localhost:${PORTA}`);
 })
+
+
+
